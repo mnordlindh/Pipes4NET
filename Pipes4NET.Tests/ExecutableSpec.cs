d49c79fd0bd444accba1bb0b45ba3b5b9a401f5d
unknown <mattiasnordlindh@gmail.com>
2014-08-21 15:18:46 +0200

    Tests for caching and lazy evaluation for the executable base class.
    Includes a implementation of a test-spy-executable.

---
+++ Pipes4NET.Tests/ExecutableSpec.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Pipes4NET.Tests {
+    [TestClass]
+    public class ExecutableSpec {
+        [TestMethod]
+        public void GetItem_ShouldCacheItems() {
+            // Setup
+            var items = Enumerable.Range(0, 10);
+            var spy = new SpyExecutable<int>();
+
+            // Act
+            var expr =
+                items
+                    .Pipe(spy)
+                    .Pipe(new IdentityExecutable<int>());
+
+            // execute the enumerator twice
+            expr.ToList();
+            expr.ToList();
+
+            // Assert that the mapper function is hit
+            // only once per item for the spy
+            var allItemsHitOnce = spy.HitCount.All(kvp => kvp.Value == 1);
+
+            Assert.IsTrue(allItemsHitOnce);
+        }
+
+        [TestMethod]
+        public void Pipe_ShouldReturnLazyExpression() {
+            // Setup
+            var items = Enumerable.Range(0, 10);
+            var spy = new SpyExecutable<int>();
+
+            // Act
+            // Build the pipe-expression but do not execute it
+            items
+                .Pipe(spy)
+                .Pipe(new IdentityExecutable<int>());
+
+            // Assert
+            // The mapper function should not be called at all
+            Assert.AreEqual(0, spy.HitCount.Count);
+        }
+
+        [TestMethod]
+        public void Pipe_ShouldReturnLazyExpression_WhichToListCanExecute() {
+            // Setup
+            var items = Enumerable.Range(0, 10);
+            var spy = new SpyExecutable<int>();
+
+            // Act
+            // Build the pipe-expression but do not execute it
+            items
+                .Pipe(spy)
+                .Pipe(new IdentityExecutable<int>())
+                .ToList();
+
+            // Assert
+            // The mapper function should not be called at all
+            Assert.AreEqual(10, spy.HitCount.Count);
+        }
+    }
+}
---
+++ Pipes4NET.Tests/SpyExecutable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pipes4NET.Tests {
+    internal class SpyExecutable<TIn> : Executable<TIn, TIn> {
+        internal Dictionary<TIn, int> HitCount = new Dictionary<TIn, int>();
+
+        public override TIn Mapper(TIn input) {
+            if (HitCount.ContainsKey(input)) {
+                HitCount[input]++;
+            } else {
+                HitCount[input] = 1;
+            }
+
+            return input;
+        }
+    }
+}
