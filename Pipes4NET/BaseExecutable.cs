299fb1f249e2f367e18e1b13d7df4cd2bb63dfbd
unknown <mattiasnordlindh@gmail.com>
2014-08-22 01:14:37 +0200

    Code cleanup.

--- Pipes4NET/BaseExecutable.cs
+++ Pipes4NET/BaseExecutable.cs
@@ -9,28 +9,33 @@ namespace Pipes4NET {
         protected IExecutable<TInput> _input;
         protected IEnumerator<TInput> _inputEnumerator;
         protected IDictionary<int, TOutput> _cache;
+        protected bool _inputIsSet = false;
+
+        protected bool hasInput {
+            get { return _input != null || _inputEnumerator != null; }
+        }
 
         public BaseExecutable() {
             _cache = new Dictionary<int, TOutput>();
         }
 
         public virtual void SetInput(IEnumerable input) {
-            if (_input == null) {
+            if (!this.hasInput) {
                 IEnumerable<TInput> i = (IEnumerable<TInput>)input;
-                if (input is IExecutable)
+
+                if (input is IExecutable<TInput>) {
                     _input = (IExecutable<TInput>)input;
+                }
                 _inputEnumerator = i.GetEnumerator();
             } else {
-                // if this executable already has a source, recursively trawel
-                // to the first executable in the chain and set its input
-                _input.SetInput(input);
+                if (_input != null) {
+                    // if this executable already has a source, recursively trawel
+                    // to the first executable in the chain and set its input
+                    _input.SetInput(input);
+                } else {
+                    throw new InvalidOperationException("This pipeline already has an initial source!");
+                }
             }
-        }
-
-        public IExecutable Pipe(IExecutable executable) {
-            executable.SetInput(this);
-
-            return executable;
         }
 
         public IExecutable<TOut> Pipe<TOut>(IExecutable<TOutput, TOut> executable) {
@@ -40,11 +45,6 @@ namespace Pipes4NET {
         }
 
         public IEnumerable<TOutput> Exec() {
-            //IEnumerator iterator = this.GetEnumerator();
-            //while (iterator.MoveNext()) {
-            //    yield return (TOutput)iterator.Current;
-            //}
-            // the foreach will iterate better then above..
             foreach (var item in this) {
                 yield return item;
             }
@@ -58,5 +58,6 @@ namespace Pipes4NET {
 
         // Caching
         public abstract bool GetItem(int index, out TOutput item);
+
     }
 }
