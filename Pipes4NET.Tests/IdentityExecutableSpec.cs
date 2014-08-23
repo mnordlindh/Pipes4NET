ba38c9c3fc22e97715ad621c02a238070101c135
unknown <mattiasnordlindh@gmail.com>
2014-08-24 00:16:30 +0200

    Tests for identity-executable.

---
+++ Pipes4NET.Tests/IdentityExecutableSpec.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Pipes4NET.Tests {
+    [TestClass]
+    public class IdentityExecutableSpec {
+        [TestMethod]
+        public void Mapper_ShouldReturnArgument() {
+            var id = new IdentityExecutable<object>();
+            var obj = new object();
+            var res = id.Mapper(obj);
+
+            Assert.AreEqual(obj, res);
+        }
+    }
+}
