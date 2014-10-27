72d541f09cd70597c8845a006dd05627215eb0fc
unknown <mattiasnordlindh@gmail.com>
2014-10-27 01:26:02 +0100

    Adds FindFiles executable.

---
+++ Pipes4NET.Tests/IO/FindFilesSpec.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pipes4NET.IO;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace Pipes4NET.Tests.IO {
+    [TestClass]
+    public class FindFilesSpec {
+        [TestMethod]
+        public void Mapper() {
+            var readFiles = new FindFiles("*.txt");
+            var dir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "/../../files";
+            var path = dir + "/file.txt";
+            Directory.CreateDirectory(dir);
+            File.Create(path);
+
+            var res = readFiles.Mapper(dir);
+
+            Assert.AreEqual(1, res.Count());
+        }
+    }
+}
---
+++ Pipes4NET/IO/FindFiles.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pipes4NET.IO {
+    public class FindFiles : Executable<string, IEnumerable<string>> {
+        protected string _pattern;
+        protected SearchOption _option = SearchOption.TopDirectoryOnly;
+            
+        public FindFiles(string pattern) {
+            _pattern = pattern;
+        }
+
+        public FindFiles(string pattern, SearchOption option)
+            : this(pattern) {
+            _option = option;
+        }
+
+        public override IEnumerable<string> Mapper(string input) {
+            if (input == null) return null;
+
+            return MiscIO.GetFiles(input, _pattern, _option);
+        }
+    }
+}
